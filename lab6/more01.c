561714b72fcc70f7d8069a43020a95c941691682
Hyun <53088084+gusdndl@users.noreply.github.com>
2023-10-19 11:09:31 +0900

    Add files via upload

--- lab6/more01.c
+++ lab6/more01.c
@@ -29,14 +29,10 @@ void do_more(FILE *fp) {
     char line[LINELEN];
     int num_of_lines = 0;
     int see_more(), reply;
-    FILE* fp_tty;
-    fp_tty = fopen("/dev/tty", "r");
-    if(fp_tty == NULL) {
-        exit(1);
-    }
+    
     while(fgets(line, LINELEN, fp)) {
         if(num_of_lines == PAGELEN) {
-            reply = see_more(fp_tty);
+            reply = see_more();
             if(reply == 0) {
                 break;
             }
--- lab6/more03.c
+++ lab6/more03.c
@@ -30,26 +30,21 @@ int main(int argc, char* argv[]) {
 }
 
 int set_screen_size() {
-    struct winsize wbuf;
-    if(ioctl(0, TIOCGWINSZ, &wbuf) != -1) {
-        return wbuf.ws_row;
-    } else {
-        return 24;
-    }
+    int fd;
+    fd = fileno(stdin);
+    struct winsize ws;
+    ioctl(fd, TIOCGWINSZ, &ws);
+    return ws.ws_row;
 }
 
 void do_more(FILE *fp) {
     char line[LINELEN];
     int num_of_lines = 0;
     int see_more(), reply;
-    FILE* fp_tty;
-    fp_tty = fopen("/dev/tty", "r");
-    if(fp_tty == NULL) {
-        exit(1);
-    }
+
     while(fgets(line, LINELEN, fp)) {
         if(num_of_lines == PAGELEN) {
-            reply = see_more(fp_tty);
+            reply = see_more();
             if(reply == 0) {
                 break;
             }
